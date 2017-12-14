9c36c1d1a5a0a6dbc871205f34af1c96bca213b3
Davide Moro <dmoro@tierratelematics.com>
2017-12-14 11:35:31 +0100

    add tests

--- tests/test_executors.py
+++ tests/test_executors.py
@@ -35,3 +35,23 @@ def test_splinter_executor_locator_bad(dummy_executor):
         dummy_executor.locator_translate(
             {'type': 'css selectorXX',
              'value': 'body'}) == ('css', 'body')
+
+
+def test_splinter_execute(dummy_executor):
+    execute_command_mock = mock.MagicMock()
+    dummy_executor.execute_command = execute_command_mock
+
+    json_data = {
+        'steps': [
+            {'type': 'get', 'url': 'http://1'},
+            {'type': 'get', 'url': 'http://2'}
+        ]
+    }
+    dummy_executor.execute(json_data)
+
+    calls = [
+        mock.call(json_data['steps'][0]),
+        mock.call(json_data['steps'][1]),
+    ]
+    assert dummy_executor.execute_command.assert_has_calls(
+        calls, any_order=False) is None
