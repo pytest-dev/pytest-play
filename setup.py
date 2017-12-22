f50df469b5dcacc82150b060fa3b2cfe4abb5967
Davide Moro <dmoro@tierratelematics.com>
2017-12-22 12:54:58 +0100

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ docs_require = [
 
 setup(
     name='pytest-play',
-    version='0.1.0',
+    version='0.1.1.dev0',
     author='Davide Moro',
     author_email='davide.moro@gmail.com',
     maintainer='Davide Moro',
