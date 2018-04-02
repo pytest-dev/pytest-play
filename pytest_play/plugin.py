1461abd147dee14e7dd1bf103812c1945e1aa058
Davide Moro <dmoro@tierratelematics.com>
2018-04-02 22:25:47 +0200

    remove dynamic markers code (future feature)

--- pytest_play/plugin.py
+++ pytest_play/plugin.py
@@ -39,7 +39,6 @@ class JSONItem(pytest.Item):
         self._setup_request()
         self._setup_play()
         self._setup_raw_data()
-        # self._setup_markers()
 
     def _setup_request(self):
         self.fixture_request = _setup_fixtures(self)
@@ -49,13 +48,6 @@ class JSONItem(pytest.Item):
 
     def _setup_raw_data(self):
         self.raw_data = self.play_json.get_file_contents(self.path)
-
-    def _setup_markers(self):
-        data = self.play_json.parametrizer.json_loads(self.raw_data)
-        for marker in data.get('markers', []):
-            self.session.config.addinivalue_line(
-                "markers", "{}: {}".format(marker, 'dynamic marker'))
-            self.add_marker(marker)
 
     def runtest(self):
         data = self.play_json.get_file_contents(self.path)
