9d8b7d1a1cded7cb02cc56f6b737e6952d2ba47e
Davide Moro <dmoro@tierratelematics.com>
2018-04-06 00:44:20 +0200

    Preparing release 1.4.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ docs_require = [
 
 setup(
     name='pytest-play',
-    version='1.4.1.dev0',
+    version='1.4.1',
     author='Davide Moro',
     author_email='davide.moro@gmail.com',
     maintainer='Davide Moro',
